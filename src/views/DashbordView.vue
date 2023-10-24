bc65c404598a99f6fd0765c77ff4412998225045
Samuel Lima <202302913458@alunos.unifanor.edu.br>
2023-10-24 00:49:10 -0300

    Add estado e typo

--- src/views/DashbordView.vue
+++ src/views/DashbordView.vue
@@ -1,14 +1,19 @@
 <template>
   <PageWarper :namePage="pageName" iconPage="md-dashboard">
     <div class="w-full h-auto felx flex-wrap">
-      <div class="p-2 rounded-md border bg-gray-300 mt-3 w-[400px]" v-for="(user, index) in sessionUsersList" :key="index">
-        <p>{{ user._id }}</p>
-        <p>{{ user.name }}</p>
-        <p>{{ user.email }}</p>
-        <p>{{ user.permissionLevel }}</p>
+      <div
+        class="p-2 rounded-md border bg-gray-300 mt-3 w-[350px] hover:opacity-80
+          shadow-md hover:shadow-none transition-all ease-in-out duration-300"
+        v-for="(user, index) in sessionUsersList" :key="index"
+        @click="handleClickCardUser(user._id)"
+        >
+        <p><span class="font-bold">Id: </span>{{ user._id }}</p>
+        <p><span class="font-bold">Nome: </span>{{ user.name }}</p>
+        <p><span class="font-bold">Email: </span>{{ user.email }}</p>
+        <p><span class="font-bold">Nivel: </span>{{ user.permissionLevel }}</p>
       </div>
 
-      <div class="w-full rounded-lg">
+      <div class="w-full rounded-lg bg-black p-4" v-if="modal">
         
       </div>
     </div>
@@ -25,6 +30,7 @@ export default {
     PageWarper
   },
   setup() {
+    let modal = ref(false)
 
     interface User {
       _id: string;
@@ -38,13 +44,18 @@ export default {
 
     websocketService.on('usersList', (usersList: any) => {
       sessionUsersList.value = usersList
-      console.log('usersList', usersList)
     })
 
-
+    function handleClickCardUser(id: string) {
+      console.log(id)
+      modal.value = !modal.value
+    }
+    
     return {
       pageName,
-      sessionUsersList
+      sessionUsersList,
+      handleClickCardUser,
+      modal
     }
   }
 }
